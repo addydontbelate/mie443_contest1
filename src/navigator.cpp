3efa8bd647f5b3eab81f8506fa01586d70ea5ffc
Markus Treppo <MarkusTreppo@Markuss-MacBook-Pro.local>
2020-01-30 12:43:00 -0500

    testing time delay for movement

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -2,8 +2,13 @@
 
 void Navigator::move_to_goal_point(float goal_x, float goal_y) 
 {
+    //  count down timer
+    std::chrono::time_point<std::chrono::system_clock> start;
+    start = std::chrono::system_clock::now();
+    uint64_t seconds_elapsed = 0;
+
     // Loop until the robot is close to the desired x & y coordinates
-    while (abs(goal_x-rob_pos_x) > 0.1 && abs(goal_y-rob_pos_y) > 0.1) 
+    while (seconds_elapsed < 10 && abs(goal_x-rob_pos_x) > 0.1 && abs(goal_y-rob_pos_y) > 0.1) 
     {
         // Calculate the x & y increments and required angle of rotation
         float inc_x = goal_x - rob_pos_x;
@@ -21,5 +26,7 @@ void Navigator::move_to_goal_point(float goal_x, float goal_y)
             angular_vel = 0;
             linear_vel = OPEN_ENV_VEL;
         }
+
+        seconds_elapsed = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::system_clock::now()-start).count();
     }
 }
