46541fe025e149dfe51624a6e37901fc1305a2f5
Pranshu <pranshumalik14@gmail.com>
2020-02-04 15:07:12 -0500

    added bumper response code

--- include/navigator.h
+++ include/navigator.h
@@ -10,17 +10,21 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/Quaternion.h>
+#include <kobuki_msgs/BumperEvent.h>
 #include <cmath>
 #include <chrono>
+
+#define NUM_BUMPER 3 // LEFT, CENTER, RIGHT
 
 // angle conversion macros
 #define RAD2DEG(rad) ((rad) * 180./M_PI)
 #define DEG2RAD(deg) ((deg) * M_PI/180.)
 
-// velocity limits
+// velocity and movement limits
 #define FREE_ENV_VEL 0.25   // [m/s]
 #define OBST_DET_VEL 0.1    // [m/s]
-#define MAX_ANG_VEL M_PI/6  // [rad/s] 
+#define MAX_ANG_VEL M_PI/6  // [rad/s]
+#define OBST_HIT_DIST 0.2   // [m] 
 
 // direction macros
 #define FWD true
@@ -35,6 +39,7 @@ extern float rob_pos_y;
 extern float goal_pos_x;
 extern float goal_pos_y;
 extern bool bumper_hit;
+extern uint8_t bumper[NUM_BUMPER];
 
 class Navigator
 {
@@ -46,7 +51,8 @@ class Navigator
   ros::Publisher vel_pub;
   geometry_msgs::Twist rob_vel;
   void publish_move();
-
+  void move_straight(float dist, float linear_speed, bool forward);
+  
  public:
   // TODO: add bumper and obstacle avoidance to move_straight() & rotate() 
   // (as a part of nav public functions)
@@ -54,12 +60,12 @@ class Navigator
   // commands
   void stop();
   void move_to(float goal_x, float goal_y);
-  void move_straight(float dist, float linear_speed, bool forward);
   void move_right(float dist, float linear_speed, float angular_speed);
   void move_left(float dist, float linear_speed, float angular_speed);
   void rotate(float rad, float angular_speed, bool clockwise);
   void rotate_right(float angular_speed);
   void rotate_left(float angular_speed);
+  void respond_to_bump();
 
    // constructor and destructor
    Navigator(ros::NodeHandle* nh);
